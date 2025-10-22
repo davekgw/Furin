d7e860aff11a85a9c041bf8673d4585c35fdcad6
davekgw <davekoagow4@gmail.com>
2025-10-22 12:21:40 +0700

    Selfbot Furin

---
+++ settings.js
@@ -0,0 +1,303 @@
+const chalk = require("chalk")
+const fs = require("fs")
+//aumto presence update
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.autoblockmorroco = true //auto block 212 (true to on, false to off)
+global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
+global.antispam = true//auto kick spammer (true to on, false to off)
+global.status= true
+global.kirsan = false
+//////////////////////////////////////////////////////////////////////////////////
+
+//BATAS//
+//=========GLOBAL MY=========//
+global.my = {
+	yt: "", //ubah saja terserah 
+    ch: "", //ubah saja sama link channel lu
+    idch: "" //ubah idch lu
+}
+////////////////////////////////////////////////////////////////////////////////
+
+//==========SETTING DELAY JPM==========//
+global.delayJpm = 7000
+//=====================================//
+//=========UBAH BAGIAN THUMBNAIL MENU & ALLMENU==========//
+global.gif = fs.readFileSync('./data/image/yoimiya.mp4'),
+global.thumbnail = 'https://files.catbox.moe/yng1lr.jpg', //THUMB MENU KALIAN
+global.dinzmenu = 'https://files.catbox.moe/b0j8ps.jpg', //THUMB MENU button KALIAN
+/////////////////////////////////////////////////////////////////////////////////
+global.setwel = "https://img1.pixhost.to/images/8859/642894040_lily.jpg"
+
+
+/////////////////////// WELCOME SETTINGS ///////////////
+global.welcome = true
+/////////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////)/)) FAKE REPLY/FAKE QUOTED //////////////////))/
+global.replyDinzID = 'https://files.catbox.moe/9lkwtv.jpg'
+global.replydinz = 'https://files.catbox.moe/b7m6c0.jpg'
+global.replyin = 'https://files.catbox.moe/9lkwtv.jpg'
+global.replyviex = 'https://files.catbox.moe/9lkwtv.jpg'
+/////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////SETTING TAMPILAN MENU KALIAN//////////////////
+global.ig = '@davekgw' //NAMA IG LU
+global.yt = '' //NAMA YT LU, KALO GADA GAUSAH DIISI
+global.namaBot = "Selfbot" // Ganti aja
+global.namabot = "Selfbot" // Ganti serah lu
+global.footer = "Selfbot•" //Ganti aja serah
+global.ttowner = 'davekgw' //NAMA TIKTOK LU
+global.namafile = '' // Ganti aja
+global.ownername = 'Dev' //NAMA LU
+global.owner = ["6289631008798"] // SETTING JUGA DI FOLDER DATABASE 
+global.ownernomer = '6289631008798' // NOMOR LU YANG MAU JDI OWNER
+global.socialm = 'GitHub: '
+global.location = 'Indonesia' 
+global.nameCreator = 'Dev~'
+/////////////////////////////////////////////////////////////////////////////////
+
+
+
+//=================SETTING PAYMENT KALIAN===================\\
+global.nodana = '089631008798' // KOSONG KAN JIKA TIDAK ADA
+global.nogopay = '089631008798' // KOSONG KAN JIKA TIDAK ADA 
+global.noovo = '089631008798' // KOSONG KAN JIKA TIDAK ADA
+/////////////////////////////////////////////////////////////////////////////////
+
+
+
+//==============SETTING PAYMENT NAME=======================\\
+global.andana = '089631008798' // KOSONG KAN JIKA TIDAK ADA
+global.angopay = '089631008798' // KOSONG KAN JIKA TIDAK ADA
+global.anovo = '089631008798' // KOSONG KAN JIKA TIDAK ADA
+//////////////////////////////////////////////////////////////////////////////////
+
+//BATAS//
+global.settings = {
+    autoreact: true
+}
+// Pick random emoji react status
+global.emoji = [
+    "🥶",
+    "🙄",
+    "😳",
+    "😒",
+    "🥰",
+    "😎",
+    "🫣",
+    "😍",
+    "😨",
+    "😁",
+    "👀",
+    "👿",
+    "🤖",
+    "😮"
+]
+//==================SETTING BOT===========================\\
+global.botname = "Selfbot•" //NAMA BOT LU
+global.ownernumber = '6289631008798' //NOMOR LU
+global.botnumber = '6289631008798' //NOMOR LU
+global.ownername = 'Dev' //NAMA LU
+global.ownerNumber = ["6289631008798@s.whatsapp.net"] //NOMORLU
+global.wagc = "Tidak ada"
+global.wach = 'Tidak ada'
+global.saluran = "..."
+global.themeemoji = '🪀'
+global.wm = "Selfbot•"
+global.botscript = 'Buy dulu!'
+global.version = "4.1.0"
+global.packname = "Selfbot•"
+global.author = "\n\nSelfbot•"
+global.creator = "6289631008798@s.whatsapp.net"
+/////////////////////////////////////////////////////////////////////////////////
+
+
+
+//================== CPANEL FITUR ==========================\\
+global.domain = 'https://go.viexshop.web.id' // Isi Domain Lu jangan kasih tanda / di akhir link
+global.apikey = 'ptla_T8FgpXYdNWqvBZOUdX4OsIGF68uiWYd11hn36BJXdLJ' // Isi Apikey Plta Lu
+global.capikey = 'ptlc_iJTCLZmhVrfrgYZ1BaceXC1hgotj5WfGhHxMvTK0DtS' // Isi Apikey Pltc Lu
+/////////////////////////////////////////////////////////////////////////////////
+
+
+
+///////////////////Server create panel egg pm2 ///////////////////////
+global.apikey2 = 'ptla_T8FgpXYdNWqvBZOUdX4OsIGF68uiWYd11hn36BJXdLJ' // Isi Apikey Plta Lu
+global.capikey2 = 'ptlc_iJTCLZmhVrfrgYZ1BaceXC1hgotj5WfGhHxMvTK0DtS' // Isi Apikey Pltc Lu
+global.domain2 = 'https://go.viexshop.web.id' // Isi Domain Lu
+global.docker2 = "ghcr.io/cekilpedia/vip:sanzubycekil" //jangan di ubah
+global.eggsnya2 = '15' // id eggs yang dipakai
+global.location2 = '1' // id location
+/////////////////////////////////////////////////////////////////////////////////
+global.domainotp = "https://claudeotp.com/api"
+global.apikeyotp = "a395f97fe99f4fad0e790d10af518b9a"
+global.eggsnya = '15' // id eggs yang dipakai
+global.location3 = '1' // id location
+global.tekspushkon = ""
+global.tekspushkonv2 = ""
+global.tekspushkonv3 = ""
+global.tekspushkonv4 = ""
+/////////////////////////////////////////////////////////////////////////////////
+
+//BATAS//
+
+/////////////////////////////////////////////////////////////////////////////////
+global.mess = {
+wait: "Wait...",
+   error: "Maaf error",
+   success: "Nih sukses",
+   on: "ON!!", 
+   off: "OFF!!",
+   done: "Done...",
+   query: {
+       text: "Text mana?",
+       link: "Link mana?",
+   },
+   error: {
+       fitur: "Fitur error!!",
+   },
+   only: {
+       group: "Hanya bisa digunakan dalam group!!",
+       private: "Hanya bisa digunakan di private chat!!",
+       owner: "Fitur owner!!",
+       admin: "Fitur admin!",
+       badmin: "Saya harus admin!!",
+       premium: "Fitur premium!!",
+   }
+}
+//========================================\\
+global.decor = {
+	menut: '❏═┅═━–〈',
+	menub: '┊•',
+	menub2: '┊',
+	menuf: '┗––––––––––✦',
+	hiasan: '꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷',
+
+	menut: '––––––『',
+    menuh: '』––––––',
+    menub: '┊☃︎ ',
+    menuf: '┗━═┅═━––––––๑\n',
+	menua: '',
+	menus: '☃︎',
+
+	htki: '––––––『',
+	htka: '』––––––',
+	haki: '┅━━━═┅═❏',
+	haka: '❏═┅═━━━┅',
+	lopr: 'Ⓟ',
+	lolm: 'Ⓛ',
+	htjava: '❃'
+}
+
+//===========================//
+
+global.rpg = {
+    emoticon(string) {
+        string = string.toLowerCase()
+        let emot = {
+            level: '📊',
+            limit: '🎫',
+            health: '❤️',
+            exp: '✨',
+            atm: '💳',
+            money: '💰',
+            bank: '🏦',
+            potion: '🥤',
+            diamond: '💎',
+            common: '📦',
+            uncommon: '🛍️',
+            mythic: '🎁',
+            legendary: '🗃️',
+            superior: '💼',
+            pet: '🔖',
+            trash: '🗑',
+            armor: '🥼',
+            sword: '⚔️',
+            makanancentaur: "🥗",
+            makanangriffin: "🥙",
+            makanankyubi: "🍗",
+            makanannaga: "🍖",
+            makananpet: "🥩",
+            makananphonix: "🧀",
+            pickaxe: '⛏️',
+            fishingrod: '🎣',
+            wood: '🪵',
+            rock: '🪨',
+            string: '🕸️',
+            horse: '🐴',
+            cat: '🐱',
+            dog: '🐶',
+            fox: '🦊',
+            robo: '🤖',
+            petfood: '🍖',
+            iron: '⛓️',
+            gold: '🪙',
+            emerald: '❇️',
+            upgrader: '🧰',
+            bibitanggur: '🌱',
+            bibitjeruk: '🌿',
+            bibitapel: '☘️',
+            bibitmangga: '🍀',
+            bibitpisang: '🌴',
+            anggur: '🍇',
+            jeruk: '🍊',
+            apel: '🍎',
+            mangga: '🥭',
+            pisang: '🍌',
+            botol: '🍾',
+            kardus: '📦',
+            kaleng: '🏮',
+            plastik: '📜',
+            gelas: '🧋',
+            chip: '♋',
+            umpan: '🪱',
+            naga: "🐉",
+            phonix: "🦅",
+            kyubi: "🦊",
+            griffin: "🦒",
+            centaur: "🎠",
+            skata: '🧩'
+        }
+        let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+        if (!results.length) return ''
+        else return emot[results[0][0]]
+    }
+}
+
+//new
+global.prefix = ['.']
+global.sessionName = 'session' // Jangan di ubah takut nanti error
+global.hituet = 0
+//media target
+global.thum = fs.readFileSync("./data/image/thumb.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./data/image/thumb.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./data/image/thumb.jpg") //ur error pic
+global.thumb = fs.readFileSync("./data/image/thumb.jpg") //ur thumb pic
+global.filename = "Selfbot•"
+global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60' //default pp wa
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+global.keyopenai = "pk-pIWAlRroXTOAigkWdHcYvmlmgzEQXuoMWbVAaLAVZswSRbEB"
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
